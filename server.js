6395d73b68bcff4e84a2bdb268f5801d9a06d5fc
Nicholas Chen <nic562@gmail.com>
2013-01-04 12:02:11 +0800

    Update server.js

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ function mk_server(local_port, remote_addr, remote_port){
             socket.destroy();
         };
     	
-    	if (DEBUG) console.log("> New connection from ip: " + socket.remoteAddress);
+        if (DEBUG) console.log("> New connection from ip: " + socket.remoteAddress);
     	
         socket.on("data", data);
         socket.on("close", close);
@@ -63,7 +63,7 @@ for(var j=0; j<proxy_hosts.length; j++){
     var h = proxy_hosts[j];
     var lport = process.env['PORT_SERVER_' + j] || (4242 + j);
     console.log("making proxy for " + h[0] + ":" + h[1] + " on port: " + lport);
-    mk_server(lport, h[0], h[1],);
+    mk_server(lport, h[0], h[1]);
 }
 
 
@@ -74,12 +74,12 @@ var http = express();
 
 http.get('/', function(req, res){
     res.json({
-		hi: 'hello world!'
-		/*
+        hi: 'hello world!'
+        /*
         host: process.env['DOTCLOUD_TCP_SERVER_HOST'],
         port: process.env['DOTCLOUD_TCP_SERVER_PORT'],
         help: "telnet "+ process.env['DOTCLOUD_TCP_SERVER_HOST'] + " " + process.env['DOTCLOUD_TCP_SERVER_PORT'], 
-		env: process.env*/
+        env: process.env*/
     });
 });
 
